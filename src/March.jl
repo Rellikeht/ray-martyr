ab85d70e0de23415b1f64a150cdac3b470511a84
Rellikeht <michaelhemperek@gmail.com>
2024-05-29 23:06:56 +0200

    FUCK CAIROMAKIE, THE SHITTIEST SCAM THEY CAME UP WITH

--- src/March.jl
+++ src/March.jl
@@ -1,25 +1,57 @@
+# using GLMakie
+# GLMakie.activate!()
+
 module March
 using Lights
 
-using GLMakie
-GLMakie.activate!(fullscreen=true)
+using CairoMakie
+CairoMakie.activate!()
 
-function render(
-    # size::Tuple{Int, Int},
-    background::Union{String,Symbol}=:black
-)::Scene
-    scene::Scene = Scene(
-        backgroundcolor=background,
-        fulllscreen=true
-    )
+# using GLMakie
+# GLMakie.activate!()
 
-    campixel!(scene)
-    scx::Int, scy::Int = size(scene)
-    println(scx)
-    println(scy)
+# GLMakie.activate!(float=true,)
+# GLMakie.activate!(fullscreen=true)
 
-    # image!(scene, [RGBf(i / scx, j / scy, 0) for i in 1:scx, j in 1:scy])
-    return scene
-end
+const FHD = (1920, 1080)
+const HD = (1280, 720)
+
+# function initScene(
+#     # size::Tuple{Int, Int},
+#     background::Union{String,Symbol}=:black
+# )::Scene
+
+#     scene::Scene = Scene(
+#         # back,
+#         clear=true,
+#         # backgroundcolor=background,
+#         backgroundcolor=:black,
+#         # size=HD,
+#         # fulllscreen=true
+#     )
+
+#     # Makie.update_limits!(scene)
+#     # scale!(scene, 1)
+#     campixel!(scene)
+#     return scene
+# end
+
+# function render!(
+#     scene::Scene
+# )
+#     scx::Int, scy::Int = size(scene)
+#     println(scx)
+#     println(scy)
+
+#     # image!(scene, [RGBf(i / scx, j / scy, 0) for i in 1:scx, j in 1:scy])
+#     image!(scene, [(i+j)/(scx+scy) for i in 1:scx, j in 1:scy])
+#     # image!(scene, [RGBf(255, 0, 0) for i in 1:scx, j in 1:scy])
+# end
+
+# function main()::Scene
+#     scene = initScene()
+#     render!(scene)
+#     return scene
+# end
 
 end
